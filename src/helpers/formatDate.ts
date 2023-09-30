11b6f73343dcb1db987964bdb44714000605a87d
OmitNomis <simon98190@gmail.com>
2023-10-01 01:07:29 +0545

    changes in formatDate, and function for reverting date

--- src/helpers/formatDate.ts
+++ src/helpers/formatDate.ts
@@ -4,3 +4,8 @@ export const formatMillisecondToDate = (time: string) => {
   const date = moment(parseInt(time) * 1000).format("yyyy-MM-DDThh:mm");
   return date;
 };
+
+export const formatDateToMillisecond = (date: string) => {
+  const time = moment(date).unix();
+  return time;
+};
