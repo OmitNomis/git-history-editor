8c2cb0e822fc1e50e51b60be284996950f1e0297
OmitNomis <simon98190@gmail.com>
2023-10-01 01:10:07 +0545

    add seconds to date format to not lose time

--- src/helpers/formatDate.ts
+++ src/helpers/formatDate.ts
@@ -1,7 +1,7 @@
 import moment from "moment";
 
 export const formatMillisecondToDate = (time: string) => {
-  const date = moment(parseInt(time) * 1000).format("yyyy-MM-DDThh:mm");
+  const date = moment(parseInt(time) * 1000).format("yyyy-MM-DDThh:mm:ss");
   return date;
 };
 
